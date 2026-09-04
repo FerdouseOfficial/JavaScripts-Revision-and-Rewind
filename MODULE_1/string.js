5c271c29c3ac36dc171abb59d1bf95515162f8ea
FerdouseOfficial <ferdousehasan395@gmail.com>
2026-09-04 18:15:28 +0600

    strings are just nuts

--- MODULE_1/string.js
+++ MODULE_1/string.js
@@ -7,3 +7,11 @@ let lastName = "Molla";
 
 let fullName = `${firstName} ${lastName}`;
 console.log(fullName);
+
+let random = " I am a student";
+console.log(random);
+
+let address = "rajshahi";
+console.log(address.toUpperCase());
+
+console.log(random.includes("student"));
