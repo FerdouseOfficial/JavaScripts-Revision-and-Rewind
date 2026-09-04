082f0e20a8ef215d5dd25b036dfbb9a0a5b8874c
FerdouseOfficial <ferdousehasan395@gmail.com>
2026-09-04 18:06:55 +0600

    Oiling up the old exicting memories let's bang this thing up

---
+++ MODULE_1/string.js
@@ -0,0 +1,9 @@
+// string 
+
+let firstName = "Jihad";
+let lastName = "Molla";
+
+// let fullName = firstName + " " + "lastName";
+
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
---
+++ MODULE_1/variable.js
@@ -0,0 +1,14 @@
+console.log("JS Variable shikhbo");
+
+let name = "Rahim";
+name = "Karim";
+
+console.log(name)
+
+const age = 21;
+// age = 20;
+
+console.log(age);
+console.log(typeof(name), typeof(age));
+
+// ssbbnnuo= string, symbol, BigInt, Boolean,number, null, undefined, object 
